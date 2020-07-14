eab359ca2324e5cbc59dbe2bd2af7103b1adf1bf
Cga <1981907427@qq.com>
2020-07-14 20:40:22 +0800

    add article-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleCardComponent } from './article-card/article-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleCardComponent
   ],
   imports: [
     BrowserModule
