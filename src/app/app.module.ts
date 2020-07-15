5c071c1e3fd672f43db0e92c2343c970bc8f78f0
Cga <1981907427@qq.com>
2020-07-15 19:43:04 +0800

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ArticleCardComponent } from './article-card/article-card.component';
+import { AsideComponent } from './aside/aside.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleCardComponent
+    ArticleCardComponent,
+    AsideComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
